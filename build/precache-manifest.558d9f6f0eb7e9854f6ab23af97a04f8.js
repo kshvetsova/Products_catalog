5dcd4d6795babb67fe94e8d94ac2ab0b867e5356
kshvetsova <katshvetsova@gmail.com>
2021-09-16 10:16:39 +0300

    Initial commit

---
+++ build/precache-manifest.558d9f6f0eb7e9854f6ab23af97a04f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c27ec3409eecac43d4fd568247058c96",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b31a6b7f9d6ae3b701fb",
+    "url": "./static/css/main.78b898ab.chunk.css"
+  },
+  {
+    "revision": "0dd0766c479c514c1eb9",
+    "url": "./static/js/2.7f13ae1c.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "./static/js/2.7f13ae1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b31a6b7f9d6ae3b701fb",
+    "url": "./static/js/main.ae26e815.chunk.js"
+  },
+  {
+    "revision": "1610068e25a4810046d0",
+    "url": "./static/js/runtime-main.e1f50cee.js"
+  }
+]);
